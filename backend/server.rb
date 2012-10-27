a2723651d4c2211b1d20da5d8e2ef01cf74c717d
zhf <hf@zhanghongfeng.com>
2012-10-28 03:15:15 +0800

    I changed a lot of things

--- backend/server.rb
+++ backend/server.rb
@@ -4,20 +4,24 @@ current_url_map = Hash.new
 
 def gen_key(params)
   user = params[:user]
-  meeting = params[:meeting]
-  "%s@%s" % [user, meeting]
+  topic = params[:topic]
+  "%s@%s" % [user, topic]
 end
 
-put '/:user/:meeting' do
+put '/:user/:topic' do
   response.headers['Access-Control-Allow-Methods'] = '"Access-Control-Allow-Methods" ":" PUT'
   url = params[:url]
   key = gen_key(params)
   current_url_map[key] = url
 end
 
-get '/:user/:meeting' do
+get '/:user/:topic' do
   response.headers['Access-Control-Allow-Origin'] = '*'
   key = gen_key(params)
   url = current_url_map[key]
-  url ||= "Not Found"
+  url ||= "Not Found" # FIXME
 end
+
+# get '/:user/now' do
+#   redirect last_topic, 303
+# end
--- developer/fowllow.js
+++ developer/fowllow.js
@@ -1,18 +1,78 @@
 var http = new XMLHttpRequest();
+var default_endpoint = "http://fowllow.com/"
 
-function put_url(name, meeting, url) {
-  http.open('PUT', 'http://localhost:4567/' + name + "/" + meeting + "?url=" + url, false);
-  http.send('url=' + url);
-}
-
-function get_url(name, meeting, callback) {
-  http.open('GET', 'http://localhost:4567/' + name + "/" + meeting, true);
-  http.onreadystatechange=function() {
-    if (http.readyState==4 && http.status==200) {
-      if (callback) {
-        callback(http.responseText);
-      };
+function get_meta(name)
+ {
+    var metas = document.getElementsByTagName('meta');
+    var i;
+    for (i = 0; i < metas.length; i++) {
+        if (metas[i].getAttribute('name') == name)
+        return metas[i].getAttribute('content');
     }
-  }
-  http.send();
+    return null;
 }
+
+var fowllow_user = get_meta('fowllow_user');
+var fowllow_topic = get_meta('fowllow_topic');
+var fowllow_endpoint = get_meta('fowllow_endpoint');
+var fowllow_role = get_meta('fowllow_role');
+
+if (!fowllow_endpoint) { fowllow_endpoint = default_endpoint}
+
+// Guide
+function report() {
+    put_url(fowllow_user, fowllow_topic, window.location);
+}
+
+function put_url(name, topic, url) {
+    http.open('PUT', fowllow_endpoint + name + "/" + topic + "?url=" + url, false);
+    // http.send('url=' + url);
+    http.send();
+}
+
+// Follower
+function get_url(name, topic, callback) {
+    http.open('GET', fowllow_endpoint + name + "/" + topic, true);
+    http.onreadystatechange = function() {
+        if (http.readyState == 4 && http.status == 200) {
+            if (callback) {
+                callback(http.responseText);
+            };
+        }
+    }
+    http.send();
+}
+
+function url_callback(url) {
+    if (url != window.location) {
+        window.location = url;
+    }
+}
+function try_to_refresh() {
+    get_url(fowllow_user, fowllow_topic, url_callback);
+}
+
+function poll() {
+    setInterval(try_to_refresh, 500);
+}
+
+var role;
+
+// Debug routings
+var isSafari = Object.prototype.toString.call(window.HTMLElement).indexOf('Constructor') > 0;
+
+if (isSafari) {
+    role = "Guide"
+    report();
+} else {
+    role = "Follower"
+    poll();
+}
+// switch (get_meta("role")) {
+// 	case "guide":
+// 		report(); //Not sure if window location is available now
+// 		break;
+// 	case "follower":
+// 		poll();
+// 		break;
+// }
